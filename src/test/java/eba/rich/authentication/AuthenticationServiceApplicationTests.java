b590797f2cb16e26066cdd381918bb288685b4d0
mehmet ali sakallı <mhmtalisklli@gmail.com>
2025-06-09 20:54:10 +0300

    first commit

---
+++ src/test/java/eba/rich/authentication/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eba.rich.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
